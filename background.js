f1f76fe2324d8f6887cec363799b38ac945e24ef
Adil Akbar <adilak389@gmail.com>
2022-08-05 09:54:45 +0400

    Update background.js

--- background.js
+++ background.js
@@ -4,30 +4,9 @@ chrome.runtime.onMessage.addListener(
         var html = request.htmlCode;
         console.log(url);
         console.log(html);
-        // console.log(sender.tab ? "from a content script: " + sender.tab.url : "from the extension");
         sendResponse({farewell: 'Got the URL and Source Code, thanks!!'});
-        
-
         chrome.runtime.sendNativeMessage('ae.gits.mohre.automation',
         { "create_file": true, "url": url, "html": html }, function(response){
             console.log(response);
         });
-
-        // chrome.runtime.sendNativeMessage('ae.gits.mohre.automation',
-        // { "create_file": true, "url": url, "html": html },
-        // function(response) {
-        //     console.log("Received " + response);
-        // });
-
-        
-
-        // tabId = sender.tab.id;
-        // var hostName = "ae.gits.mohre.automation";
-        // port = browser.runtime.connectNative(hostName);
-        // message = {"update": "Mohre Site Data", "url": url, "html": html};
-        // port.postMessage(message);
-        // port.onMessage.addListener((response) => {
-        //     browser.tabs.sendMessage(tabId, { action: "TASHEEL_UPDATE_TRANSACTION_EVENT", response: response }, function (response) { });
-        // });
-        // port.onDisconnect.addListener(() => {port = null;});
 });
